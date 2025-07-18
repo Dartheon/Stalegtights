c8817acf6569b169f4e3b9ecc8955e69a518b6d5
Dartheon <brian_Sutherland100@hotmail.com>
2025-07-17 20:56:43 -0600

    Added Wall and Climbing State Script
    Added basic scripts to the Wall State and Climbing State to be filled in later

--- StalegtightsGodotProject/Scripts/ClimbingState.cs
+++ StalegtightsGodotProject/Scripts/ClimbingState.cs
@@ -1,6 +1,156 @@
 using Godot;
-using System;
 
 public partial class ClimbingState : States
 {
+    #region Variables
+    #region DEBUG
+    //DEBUG Variables Go Here...
+    #endregion
+
+    #region General
+    /*References from StateMachine
+    Player              - CharacterBody2D
+    PlayerScript        - Player
+    Gravity             - Float
+    PlayerJumpVelocity  - Float
+    */
+
+    /*Variables from StateMachine that need direct Referenece
+    stateMachine.smPlayerVelocity     - Vector2
+    */
+    #endregion
+
+    #region Animations
+    //
+    #endregion
+
+    #region Movement
+    //
+    #endregion
+    #endregion
+
+    public override void Start()
+    {
+        #region DEBUG
+        //DEBUG Variables Go Here...
+        #endregion
+
+        #region General
+        //
+        #endregion
+
+        #region Animations
+        //
+        #endregion
+
+        #region Movement
+        //
+        #endregion
+    }
+
+    public override void Enter()
+    {
+        //set up stuff when entering this state
+        #region DEBUG
+        //DEBUG Variables Go Here...
+        GD.Print($"Entering {CLIMBINGSTATESTRING} from {PreviousState}");
+        #endregion
+
+        #region General
+        NewStateChange = CLIMBINGSTATESTRING;
+        #endregion
+
+        #region Animations
+        //
+        #endregion
+
+        #region Movement
+        //
+        #endregion
+    }
+
+    public override void Update(double delta)
+    {
+        #region DEBUG
+        //DEBUG Variables Go Here...
+        #endregion
+
+        #region Animations
+        //
+        #endregion
+    }
+
+    public override void HandleContinuousInput(double delta)
+    {
+        #region DEBUG
+        //DEBUG Variables Go Here...
+        #endregion
+
+        #region General
+        //
+        #endregion
+
+        #region Animations
+        //
+        #endregion
+
+        #region Movement
+        //
+        #endregion
+    }
+
+    public override void PhysicsUpdate(double delta)
+    {
+        #region DEBUG
+        //DEBUG Variables Go Here...
+        #endregion
+
+        #region Movement
+
+        if (NewStateChange != CLIMBINGSTATESTRING)
+        {
+            StateMachineScript.TransitionToState(NewStateChange);
+        }
+        #endregion
+    }
+
+    public override void HandleInput(InputEvent @event)
+    {
+        #region DEBUG
+        //DEBUG Variables Go Here...
+        #endregion
+
+        #region General
+        //
+        #endregion
+
+        #region Animations
+        //
+        #endregion
+
+        #region Movement
+        //
+        #endregion
+    }
+
+    public override void Exit()
+    {
+        //clean up this state when moving out of it
+        #region DEBUG
+        //DEBUG Variables Go Here...
+        GD.Print($"Exiting  {CLIMBINGSTATESTRING}");
+        #endregion
+
+        #region General
+        NewStateChange = "";
+        #endregion
+
+        #region Animations
+        //
+        #endregion
+
+        #region Movement
+        //
+        #endregion
+    }
 }
--- StalegtightsGodotProject/Scripts/WallState.cs
+++ StalegtightsGodotProject/Scripts/WallState.cs
@@ -1,6 +1,201 @@
 using Godot;
-using System;
 
 public partial class WallState : States
 {
+    #region Variables
+    #region DEBUG
+    //DEBUG Variables Go Here...
+    #endregion
+
+    #region General
+    /*References from StateMachine
+    Player              - CharacterBody2D
+    PlayerScript        - Player
+    Gravity             - Float
+    PlayerJumpVelocity  - Float
+    */
+
+    /*Variables from StateMachine that need direct Referenece
+    stateMachine.smPlayerVelocity     - Vector2
+    */
+    #endregion
+
+    #region Animations
+    //
+    #endregion
+
+    #region Movement
+    //
+    #endregion
+    #endregion
+
+    public override void Start()
+    {
+        #region DEBUG
+        //DEBUG Variables Go Here...
+        #endregion
+
+        #region General
+        //
+        #endregion
+
+        #region Animations
+        //
+        #endregion
+
+        #region Movement
+        //
+        #endregion
+    }
+
+    public override void Enter()
+    {
+        //set up stuff when entering this state
+        #region DEBUG
+        //DEBUG Variables Go Here...
+        GD.Print($"Entering {WALLSTATESTRING}     from {PreviousState}");
+        #endregion
+
+        #region General
+        NewStateChange = WALLSTATESTRING;
+        #endregion
+
+        #region Animations
+        //
+        #endregion
+
+        #region Movement
+        //
+        #endregion
+    }
+
+    public override void Update(double delta)
+    {
+        #region DEBUG
+        //DEBUG Variables Go Here...
+        #endregion
+
+        #region Animations
+        //
+        #endregion
+    }
+
+    public override void HandleContinuousInput(double delta)
+    {
+        #region DEBUG
+        //DEBUG Variables Go Here...
+        #endregion
+
+        #region General
+        //
+        #endregion
+
+        #region Animations
+        //
+        #endregion
+
+        #region Movement
+        #region Check to see if still on wall - if character collider is still interacting with the wall
+        //
+        #endregion
+
+        #region Detect power slide out - add code
+        //
+        #endregion
+
+        #region Detect for power slide - add code
+        //
+        #endregion
+        #endregion
+    }
+
+    public override void PhysicsUpdate(double delta)
+    {
+        #region DEBUG
+        //DEBUG Variables Go Here...
+        #endregion
+
+        #region Movement
+
+        #region Character Stun - add code
+        //
+        #endregion
+
+        #region Touching the Wall
+        //
+        #endregion
+
+        #region Touching the Ground
+        //
+        #endregion
+
+        #region Process Sliding Down the Wall
+        //
+        #endregion
+
+        #region Change State Logic
+        if (NewStateChange != WALLSTATESTRING)
+        {
+            StateMachineScript.TransitionToState(NewStateChange);
+        }
+        #endregion
+        #endregion
+    }
+
+    public override void HandleInput(InputEvent @event)
+    {
+        #region DEBUG
+        //DEBUG Variables Go Here...
+        #endregion
+
+        #region General
+        //
+        #endregion
+
+        #region Animations
+        //
+        #endregion
+
+        #region Movement
+        #region Detect Jumping Off
+        //
+        #endregion
+
+        #region Detect Diving Out - add code
+        //
+        #endregion
+
+        #region Detect if Character is Dropping Down - add code
+        //
+        #endregion
+
+        #region Change State Logic
+        if (NewStateChange != WALLSTATESTRING)
+        {
+            StateMachineScript.TransitionToState(NewStateChange);
+        }
+        #endregion
+        #endregion
+    }
+
+    public override void Exit()
+    {
+        //clean up this state when moving out of it
+        #region DEBUG
+        //DEBUG Variables Go Here...
+        GD.Print($"Exiting  {WALLSTATESTRING}");
+        #endregion
+
+        #region General
+        NewStateChange = "";
+        #endregion
+
+        #region Animations
+        //
+        #endregion
+
+        #region Movement
+        //
+        #endregion
+    }
 }
