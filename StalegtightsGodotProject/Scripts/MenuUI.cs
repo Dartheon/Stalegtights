04bc1bbd71c19d4d8cb125e099d4f874cd7bcd6e
Dartheon <brian_Sutherland100@hotmail.com>
2025-12-12 00:51:06 -0700

    Added Menu Code
    Added in a bunch of code for the menus to get a start. New Game will hide the Start Menu. Quit To Desktop will close game.

--- StalegtightsGodotProject/Scripts/MenuUI.cs
+++ StalegtightsGodotProject/Scripts/MenuUI.cs
@@ -2,5 +2,104 @@ using Godot;
 
 public partial class MenuUI : Control
 {
-    //Menu Code Logic...
+    #region Variables
+    private Control pauseMenu;
+    private Control startMenu;
+    private Control mainMenu;
+    #endregion
+
+    #region Methods
+    #region Start
+    public override void _Ready()
+    {
+        pauseMenu = GetNode<Control>("%PauseMenu");
+        startMenu = GetNode<Control>("%StartMenu");
+        mainMenu = GetNode<Control>("%MainMenu");
+
+        //Setting Initial Visibility
+        pauseMenu.Visible = false;
+        startMenu.Visible = true;
+        mainMenu.Visible = false;
+    }
+    #endregion
+
+    #region Process
+    public override void _Process(double delta)
+    {
+        //
+    }
+
+    #endregion
+
+    #region General Buttons Signals
+    public void OnQuitToDesktopPressed()
+    {
+        //Add code here...
+        //StartMenu
+        //MainMenu
+        GD.Print("Pressed 'QuitToDesktop' Button");
+        GetTree().Quit();
+    }
+
+    public void OnOptionsPressed()
+    {
+        //Add code here...
+        //StartMenu
+        //MainMenu
+        GD.Print("Pressed 'Options' Button");
+    }
+
+    public void OnCloseMenuPressed()
+    {
+        //Add code here...
+        //Have some way to close all menus
+        //MainMenu
+        //PauseMenu
+        GD.Print("Pressed 'CloseMenu' Button");
+    }
+    #endregion
+
+    #region Start Menu Button Signals
+    public void OnNewGamePressed()
+    {
+        //Add code here...
+        GD.Print("Pressed 'NewGame' Button");
+        startMenu.Visible = false;
+    }
+
+    public void OnLoadGamePressed()
+    {
+        //Add code here...
+        GD.Print("Pressed 'LoadGame' Button");
+    }
+    #endregion
+
+    #region Main Menu Button Signals
+    public void OnQuitToStartMenuPressed()
+    {
+        //Add code here...
+        GD.Print("Pressed 'QuitToStartMenu' Button");
+    }
+    #endregion
+
+    #region Pause Menu Button Signals
+    public void OnInventoryPressed()
+    {
+        //Add code here...
+        GD.Print("Pressed 'Inventory' Button");
+    }
+
+    public void OnMapPressed()
+    {
+        //Add code here...
+        GD.Print("Pressed 'Map' Button");
+    }
+
+    public void OnCodexPressed()
+    {
+        //Add code here...
+        GD.Print("Pressed 'Codex' Button");
+    }
+    #endregion
+    #endregion
 }
