89a78bc33f85af5f296eda731a11a73e7d85d0f7
Dartheon <brian_Sutherland100@hotmail.com>
2025-12-18 22:30:38 -0700

    Menu Script Logic
    Logic added for opening and closing menus hardcoded based on the game state.
    Main Menu is set to 'ESC'
    Pause is set to 'P'

--- StalegtightsGodotProject/Scripts/InputManager.cs
+++ StalegtightsGodotProject/Scripts/InputManager.cs
@@ -11,23 +11,28 @@ public partial class InputManager : Node
     public Dictionary<string, bool> PlayerInputBuffers { get; set; } = new()
     {
         //Player Inputs That Require a Timer to Reset
-        { "jump", false},
+        { "jump", false },
     };
 
     //These inputs are for continuous key presses that the bool needs to be reset in the code
     public Dictionary<string, bool> PlayerContinuousInputs { get; set; } = new()
     {
         //Player Inputs That Get Reset In the Code
-        { "move_right", false},
-        { "move_left", false},
-        { "interact", false},
-        { "duck", false},
-        { "crawling_left", false},
-        { "crawling_right", false},
+        { "move_right", false },
+        { "move_left", false },
+        { "interact", false },
+        { "duck", false },
+        { "crawling_left", false },
+        { "crawling_right", false },
+
+        //Menu
+        { "pause_menu", false },
+        { "main_menu",false },
+        
         //Debug Inputs
-        { "engine_scale_up", false},
-        { "engine_scale_down", false},
-        { "engine_scale_reset", false},
+        { "engine_scale_up", false },
+        { "engine_scale_down", false },
+        { "engine_scale_reset", false },
     };
 
     public Timer JumpTimer { get; set; }
@@ -91,6 +96,12 @@ public partial class InputManager : Node
         #region General
         //Player Interact
         PlayerContinuousInputs["interact"] = Input.IsActionJustPressed("interact") && (GameManager.InteractablesEntered?.Count > 0);
+
+        //Main Menu
+        PlayerContinuousInputs["main_menu"] = Input.IsActionJustPressed("main_menu");
+
+        //Pause Menu
+        PlayerContinuousInputs["pause_menu"] = Input.IsActionJustPressed("pause_menu");
         #endregion
 
         #region Debug
--- StalegtightsGodotProject/Scripts/MenuUI.cs
+++ StalegtightsGodotProject/Scripts/MenuUI.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Security.Cryptography;
 using Godot;
 
 public partial class MenuUI : Control
@@ -8,6 +10,7 @@ public partial class MenuUI : Control
     private Control mainMenu;
 
     private GameManager gameManager;
+    private InputManager inputManager;
     private CharacterBody2D playerCB2D;
     #endregion
 
@@ -16,6 +19,7 @@ public partial class MenuUI : Control
     public override void _Ready()
     {
         gameManager = GetNode<GameManager>("/root/GameManager");
+        inputManager = GetNode<InputManager>("/root/InputManager");
 
         pauseMenu = GetNode<Control>("%PauseMenu");
         startMenu = GetNode<Control>("%StartMenu");
@@ -31,7 +35,63 @@ public partial class MenuUI : Control
     #region Process
     public override void _Process(double delta)
     {
-        //
+        //Main Menu Logic
+        if (inputManager.PlayerContinuousInputs["main_menu"])
+        {
+            inputManager.PlayerContinuousInputs["main_menu"] = false;
+
+            //if menu is not visible make it visible, otherwise close menu
+            if (!mainMenu.Visible)
+            {
+                gameManager.PauseManager(true);
+                mainMenu.Visible = true;
+            }
+            else
+            {
+                //If Controller: Pause -> Main -> Main = Close Both Menus
+                //mainMenu.Visible = false; pauseMenu.Visible = false;
+
+                //If Keyboard:   Pause -> Main -> Main = Close Main Menu
+                mainMenu.Visible = false;
+
+                if (!mainMenu.Visible && !pauseMenu.Visible && !startMenu.Visible)
+                {
+                    gameManager.PauseManager(false);
+                }
+            }
+        }
+
+        //Pause Menu Logic
+        if (inputManager.PlayerContinuousInputs["pause_menu"])
+        {
+            inputManager.PlayerContinuousInputs["pause_menu"] = false;
+
+            //from game to pause, no main
+            if (!pauseMenu.Visible && !mainMenu.Visible)
+            {
+                gameManager.PauseManager(true);
+                pauseMenu.Visible = true;
+            }
+            //from pause to game, no main
+            else if (pauseMenu.Visible && !mainMenu.Visible)
+            {
+                pauseMenu.Visible = false;
+                gameManager.PauseManager(false);
+            }
+            //from main no pause, then pause
+            else if (!pauseMenu.Visible && mainMenu.Visible)
+            {
+                gameManager.PauseManager(true);
+                pauseMenu.Visible = true;
+                mainMenu.Visible = false;
+            }
+            //from pause to main, then pause again in main
+            else
+            {
+                gameManager.PauseManager(true);
+                mainMenu.Visible = false;
+            }
+        }
     }
 
     #endregion
@@ -61,6 +121,21 @@ public partial class MenuUI : Control
         //MainMenu
         //PauseMenu
         GD.Print("Pressed 'CloseMenu' Button");
+
+        if (mainMenu.Visible)
+        {
+            mainMenu.Visible = false;
+        }
+
+        if (pauseMenu.Visible)
+        {
+            pauseMenu.Visible = false;
+        }
+
+        if (!mainMenu.Visible && !pauseMenu.Visible)
+        {
+            gameManager.PauseManager(false);
+        }
     }
     #endregion
 
